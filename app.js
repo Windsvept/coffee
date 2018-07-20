66c1c8d0a928839f4033fb562e75ffdfd537a045
Jun Sub Choi <junsubjjang@gmail.com>
2018-07-20 10:58:21 -0700

    Initialized

---
+++ app.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+	//console.log('+++ filename: app.js functionName: jquery.ready expected: no error actual: ', $);
+	//attach event listener to button(input?)
+	//create function stub for read/write/delete
+		//research local storage
+	// $().click(function(){
+
+	// });
+
+	$().click(function(){
+		
+	});
+});
